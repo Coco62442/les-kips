efad154ba04635227a80f8427cf607a5fcea06f5
alexis-fondard <alexis.fondard@etu.umontpellier.fr>
2022-12-02 02:49:07 +0100

    navbar v1

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,14 @@ import BienEtre from './Pages/BienEtre';
 import Aide from './Pages/Aide';
 import Jeu from './Pages/Jeu';
 import {Routes, Route, Link, BrowserRouter} from 'react-router-dom'
+import ResponsiveAppBar from './components/navbar';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <p>Logo</p>
-        <h2>NomSite</h2>
-        <Link to='./'>Home</Link>
-        <Link to='./Sante'>Santé</Link>
-        <Link to='./BienEtre'>Bien-être</Link>
-        <Link to='./Aide'>Aide</Link>
-        <Link to='./Jeu'>Le Jeu !</Link>
+      <ResponsiveAppBar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/Sante" element={<Sante/>}/>
