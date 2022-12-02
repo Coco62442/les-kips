01b49235cff60f4ed0520af8093b68970ee0bd63
alexis-fondard <alexis.fondard@etu.umontpellier.fr>
2022-12-02 06:44:54 +0100

    sida marche

--- src/Pages/Sante.tsx
+++ src/Pages/Sante.tsx
@@ -4,20 +4,7 @@ import Sida from "../components/Mst/Sida"
 function Sante() {
     return(
         <div>
-            
-            <div>
-                <p>
-                    Page pour la santé !
-                </p>
-            </div>
-            <div id="QuiSomme">
-                <h2>IST</h2>
-            <p>Les infections sexuellement transmissibles, anciennement appelées MST, sont variées et peuvent toucher toutes personnes ayant une vie sexuelle active.
-                On vous aide ici à en connaître leurs transmissions, leurs symptômes, leurs traitements et leur dépistage</p>
-            </div>
             <Sida/>
-        
-            
         </div>
     )
 }
